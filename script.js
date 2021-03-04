f9af9008cbf2cef4bf6bb5e5d98a88d0eb8579a3
farhanalzuhri <80027812+farhanalzuhri@users.noreply.github.com>
2021-03-04 16:32:01 +0700

    perubahan ketiga
    pengulangan untuk membuat program apabila ingin lanjut atau tidak

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-alert('program memasukan angka positif dan negatif');
-let angka = prompt('masukan angka');
+let status = true;
+while ( status = true ){  
+  alert('program memasukan angka positif dan negatif')
+  let angka = parseInt(prompt('masukan angka'))
 
-if (angka >= 0) {
-  alert('angka ini positif');
-} else {
-  alert('angka ini negatif');
-}else {
-  alert('ini bukan angka');
+  if (angka >= 0) {
+    alert('angka ini positif');
+  } else if (angka == 0) {
+    alert('angka ini adalah 0');
+  } else if (angka < 0) {
+    alert('angka ini adalah negatif');
+  } else {
+    alert('ini bukan angka');
+  }
+
+  tanya = confirm('apakah anda mau memasukan ulang?')
+  
 }
